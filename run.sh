f462a702e08bde66e54f380f7e408e9f021498a5
loxleu <pocinkomik@gmail.com>
2024-04-27 22:17:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-65d26f19-c7a4-4cbb-aabe-3a93c76519a9/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://g.w.lavanet.xyz:443/gateway/eth/rpc-http/abaf964b67059aecc8fe51ce91240cd6)
 
 
 echo "$curl_output" >> Update.tsx
